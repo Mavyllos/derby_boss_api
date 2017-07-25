112c2df3d4a92dce105a2dccafefcaafbd5c1af8
Summer Barclay <evil_lyns@yahoo.com>
2017-07-24 22:36:25 -0700

    Update for CORS issue

--- app.js
+++ app.js
@@ -12,6 +12,11 @@ const bodyParser = require('body-parser');
 
 const app = express();
 app.disable('x-powered-by');
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 // app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 app.use(logger('dev'));
